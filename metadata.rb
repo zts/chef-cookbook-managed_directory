32b51e1474556de83a16652d11fd8b238eb9d45b
Zachary Stevens <zts@cryptocracy.com>
2013-07-29 22:37:14 +0100

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "zts@cryptocracy.com"
 license          "Apache 2.0"
 description      "Provides the managed_directory resource"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.0"
