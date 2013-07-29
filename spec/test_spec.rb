0d48b9128eb8d8c324fe49cf657c60dc77815088
Zachary Stevens <zts@cryptocracy.com>
2013-07-29 21:42:08 +0100

    More tests of the basic test recipe.

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -19,9 +19,27 @@ describe 'file a followed by managed_directory' do
     runner = ChefSpec::ChefRunner.new(:step_into => ['managed_directory'])
     runner.converge 'managed_directory::t_basic'
   }
+  before(:each) {
+    Dir.stub(:glob).and_call_original
+  }
+  describe 'given only file a' do
+    before(:each) {
+      Dir.should_receive(:glob).with("/tmp/foo/*").and_return(["/tmp/foo/a"])
+    }
+    it 'should not remove file a' do
+      expect(chef_run).not_to delete_file "/tmp/foo/a"
+    end
+  end
+  describe 'given file b' do
+    before(:each) {
+      Dir.should_receive(:glob).with("/tmp/foo/*").and_return(["/tmp/foo/b"])
+    }
+    it 'should remove file b' do
+      expect(chef_run).to delete_file "/tmp/foo/b"
+    end
+  end
   describe 'given files a and b' do
     before(:each) {
-      Dir.stub(:glob).and_call_original
       Dir.should_receive(:glob).with("/tmp/foo/*").and_return(["/tmp/foo/a","/tmp/foo/b"])
     }
     it 'should not remove file a' do
@@ -29,6 +47,14 @@ describe 'file a followed by managed_directory' do
     end
     it 'should remove file b' do
       expect(chef_run).to delete_file "/tmp/foo/b"
+    end
+  end
+  describe 'given no files' do
+    before(:each) {
+      Dir.should_receive(:glob).with("/tmp/foo/*").and_return([])
+    }
+    it 'should not remove file a' do
+      expect(chef_run).not_to delete_file "/tmp/foo/a"
     end
   end
 end
