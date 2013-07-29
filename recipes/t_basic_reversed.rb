4d6b4e92338eeff1c14f4e4285f6ffbe189079c5
Zachary Stevens <zts@cryptocracy.com>
2013-07-29 21:45:05 +0100

    Test that resource order doesn't matter.
    In this test, a relevant file resource is declared after the
    managed_directory resource.

---
+++ recipes/t_basic_reversed.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: managed_directory
+# Recipe:: t_basic_reversed
+#
+# used by chefspec
+
+# Define the managed_directory
+managed_directory "/tmp/foo" do
+  action :clean
+end
+
+# Create a File resource for 'a'
+file "/tmp/foo/a" do
+  action :touch
+end
+
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -58,3 +58,48 @@ describe 'file a followed by managed_directory' do
     end
   end
 end
+
+describe 'managed_directory followed by file a' do
+  let(:chef_run) {
+    runner = ChefSpec::ChefRunner.new(:step_into => ['managed_directory'])
+    runner.converge 'managed_directory::t_basic_reversed'
+  }
+  before(:each) {
+    Dir.stub(:glob).and_call_original
+  }
+  describe 'given only file a' do
+    before(:each) {
+      Dir.should_receive(:glob).with("/tmp/foo/*").and_return(["/tmp/foo/a"])
+    }
+    it 'should not remove file a' do
+      expect(chef_run).not_to delete_file "/tmp/foo/a"
+    end
+  end
+  describe 'given file b' do
+    before(:each) {
+      Dir.should_receive(:glob).with("/tmp/foo/*").and_return(["/tmp/foo/b"])
+    }
+    it 'should remove file b' do
+      expect(chef_run).to delete_file "/tmp/foo/b"
+    end
+  end
+  describe 'given files a and b' do
+    before(:each) {
+      Dir.should_receive(:glob).with("/tmp/foo/*").and_return(["/tmp/foo/a","/tmp/foo/b"])
+    }
+    it 'should not remove file a' do
+      expect(chef_run).not_to delete_file "/tmp/foo/a"
+    end
+    it 'should remove file b' do
+      expect(chef_run).to delete_file "/tmp/foo/b"
+    end
+  end
+  describe 'given no files' do
+    before(:each) {
+      Dir.should_receive(:glob).with("/tmp/foo/*").and_return([])
+    }
+    it 'should not remove file a' do
+      expect(chef_run).not_to delete_file "/tmp/foo/a"
+    end
+  end
+end
