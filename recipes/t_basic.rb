5576acae0b229b1005e80f6232928bac11e4ebcf
Zachary Stevens <zts@cryptocracy.com>
2013-07-29 21:34:20 +0100

    Add a more basic test.

---
+++ recipes/t_basic.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: managed_directory
+# Recipe:: t_basic
+#
+# used by chefspec
+
+# Create a File resource for 'a'
+file "/tmp/foo/a" do
+  action :touch
+end
+
+# Define the managed_directory
+managed_directory "/tmp/foo" do
+  action :clean
+end
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,6 +1,6 @@
 require 'chefspec'
 
-describe 'managed_directory::test' do
+describe 'the test recipe example' do
   let(:chef_run) {
     runner = ChefSpec::ChefRunner.new(:step_into => ['managed_directory'])
     runner.converge 'managed_directory::test'
@@ -11,5 +11,24 @@ describe 'managed_directory::test' do
   it 'should remove file c' do
     Dir.should_receive(:glob).with("/tmp/foo/*").and_return(["/tmp/foo/a","/tmp/foo/b","/tmp/foo/c"])
     expect(chef_run).to delete_file "/tmp/foo/c"
+  end
+end
+
+describe 'file a followed by managed_directory' do
+  let(:chef_run) {
+    runner = ChefSpec::ChefRunner.new(:step_into => ['managed_directory'])
+    runner.converge 'managed_directory::t_basic'
+  }
+  describe 'given files a and b' do
+    before(:each) {
+      Dir.stub(:glob).and_call_original
+      Dir.should_receive(:glob).with("/tmp/foo/*").and_return(["/tmp/foo/a","/tmp/foo/b"])
+    }
+    it 'should not remove file a' do
+      expect(chef_run).not_to delete_file "/tmp/foo/a"
+    end
+    it 'should remove file b' do
+      expect(chef_run).to delete_file "/tmp/foo/b"
+    end
   end
 end
