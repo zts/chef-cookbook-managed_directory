85c009be974d3ad0cb33300e571070b352c76e93
Gregory Ruiz-Ade <gkra@unnerving.org>
2015-06-29 09:05:48 -0700

    Add default spec for default recipe

---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: managed_directory
+# Spec:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'managed_directory::default' do
+  context 'When all attributes are default, on CentOS 6.6' do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new(
+        platform: 'centos',
+        version: '6.6'
+      ) do |node|
+        # Set/override attributes here
+        # node.automatic['virtualization']['role'] = 'guest'
+      end
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      chef_run # This should not raise an error
+    end
+  end
+end
