420f0086b47b280fee555287934a04ce4c8c8353
Gregory Ruiz-Ade <gkra@unnerving.org>
2015-06-29 10:01:11 -0700

    Update test recipe spec

--- spec/unit/recipes/test_spec.rb
+++ spec/unit/recipes/test_spec.rb
@@ -23,10 +23,13 @@ describe 'managed_directory::test' do
       testdir_contents = [
         '/tmp/foo/a',
         '/tmp/foo/b',
-        '/tmp/foo/c'
+        '/tmp/foo/c',
+        '/tmp/foo/c_dir'
       ]
       allow(Dir).to receive(:glob).and_call_original
       allow(Dir).to receive(:glob).with('/tmp/foo/*').and_return(testdir_contents)
+      allow(File).to receive(:directory?).and_call_original
+      allow(File).to receive(:directory?).with('/tmp/foo/c_dir').and_return(true)
     end
 
     it 'should not remove file a' do
@@ -39,6 +42,12 @@ describe 'managed_directory::test' do
 
     it 'should remove file c' do
       expect(chef_run).to delete_file('/tmp/foo/c')
+    end
+
+    # It shouldn't remove c_dir because in this test we haven't told
+    # managed_directory to clean directories.
+    it 'should not remove directory c_dir' do
+      expect(chef_run).to_not delete_directory('/tmp/foo/c_dir')
     end
   end
 end
