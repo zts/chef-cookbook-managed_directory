b961494e6863f0197b4aef8c6c1c02a6b61083de
Gregory Ruiz-Ade <gkra@unnerving.org>
2015-06-29 12:36:04 -0700

    No, really, silence serverspec for default recipe

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,16 +4,16 @@ describe 'managed_directory::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
 
-  if %w(linux redhat fedora ubuntu debian).include?(os[:family])
-    # Tests for Linux
-    # it 'does something in linux' do
-    #   skip 'Replace this with meaningful tests'
-    # end
-
-  elsif %w(windows).include?(os[:family])
-    # Tests for Windows
-    # it 'does something in windows' do
-    #   skip 'Replace this with meaningful tests'
-    # end
-  end
+  # if %w(linux redhat fedora ubuntu debian).include?(os[:family])
+  #   # Tests for Linux
+  #   it 'does something in linux' do
+  #     skip 'Replace this with meaningful tests'
+  #   end
+  #
+  # elsif %w(windows).include?(os[:family])
+  #   # Tests for Windows
+  #   it 'does something in windows' do
+  #     skip 'Replace this with meaningful tests'
+  #   end
+  # end
 end
