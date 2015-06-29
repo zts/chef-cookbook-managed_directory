4b2bdc116e0e595e5974863767a69923a36025be
Gregory Ruiz-Ade <gkra@unnerving.org>
2015-06-29 10:34:06 -0700

    test directories with /tmp/bar to avoid collisions

--- spec/unit/recipes/test_directories_spec.rb
+++ spec/unit/recipes/test_directories_spec.rb
@@ -21,42 +21,42 @@ describe 'managed_directory::test_directories' do
 
     before do
       testdir_contents = [
-        '/tmp/foo/a',
-        '/tmp/foo/a_dir',
-        '/tmp/foo/b_dir',
-        '/tmp/foo/c_dir'
+        '/tmp/bar/a',
+        '/tmp/bar/a_dir',
+        '/tmp/bar/b_dir',
+        '/tmp/bar/c_dir'
       ]
       allow(Dir).to receive(:glob).and_call_original
-      allow(Dir).to receive(:glob).with('/tmp/foo/*').and_return(testdir_contents)
+      allow(Dir).to receive(:glob).with('/tmp/bar/*').and_return(testdir_contents)
       allow(File).to receive(:directory?).and_call_original
-      allow(File).to receive(:directory?).with('/tmp/foo/a_dir').and_return(true)
-      allow(File).to receive(:directory?).with('/tmp/foo/b_dir').and_return(true)
-      allow(File).to receive(:directory?).with('/tmp/foo/c_dir').and_return(true)
+      allow(File).to receive(:directory?).with('/tmp/bar/a_dir').and_return(true)
+      allow(File).to receive(:directory?).with('/tmp/bar/b_dir').and_return(true)
+      allow(File).to receive(:directory?).with('/tmp/bar/c_dir').and_return(true)
     end
 
-    it 'should clean managed_directory /tmp/foo' do
-      expect(chef_run).to clean_managed_directory('/tmp/foo').with(
+    it 'should clean managed_directory /tmp/bar' do
+      expect(chef_run).to clean_managed_directory('/tmp/bar').with(
         clean_directories: true,
         clean_files: false
       )
     end
 
-    # The file /tmp/foo/a shouldn't be removed for this test since we've
+    # The file /tmp/bar/a shouldn't be removed for this test since we've
     # disabled cleaning files in this recipe
     it 'should not remove file a' do
-      expect(chef_run).to_not delete_file('/tmp/foo/a')
+      expect(chef_run).to_not delete_file('/tmp/bar/a')
     end
 
     it 'should not remove directory a_dir' do
-      expect(chef_run).to_not delete_directory('/tmp/foo/a_dir')
+      expect(chef_run).to_not delete_directory('/tmp/bar/a_dir')
     end
 
     it 'should not remove directory b_dir' do
-      expect(chef_run).to_not delete_directory('/tmp/foo/b_dir')
+      expect(chef_run).to_not delete_directory('/tmp/bar/b_dir')
     end
 
     it 'should remove directory c_dir' do
-      expect(chef_run).to delete_directory('/tmp/foo/c_dir')
+      expect(chef_run).to delete_directory('/tmp/bar/c_dir')
     end
   end
 end
