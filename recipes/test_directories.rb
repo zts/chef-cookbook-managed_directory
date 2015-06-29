dbd482e1491c9ee6623e6681810f77e15704ca62
Gregory Ruiz-Ade <gkra@unnerving.org>
2015-06-29 09:07:05 -0700

    add recipe and spec for directory tests

---
+++ recipes/test_directories.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: managed_directory
+# Recipe:: test
+#
+# A simple demonstration of managed_directory, also used by chefspec.
+
+testdir = "/tmp/foo"
+
+# Setup the test.
+# 1. Create the directory for our test files, during the compile phase.
+directory testdir do
+  action :nothing
+end.run_action(:create)
+
+# # 2. Put some files in it, without using Chef, before convergence
+# %w{a b c}.each do |f|
+#   ::File.new("#{testdir}/#{f}", "w+")
+# end
+
+# Create a directory resource for 'a_dir'
+directory "#{testdir}/a_dir" do
+  action :create
+end
+
+# Define the managed_directory and have it clean directories, too
+managed_directory testdir do
+  action :clean
+  clean_directories true
+end
+
+# Create a directory resource for 'b_dir'
+# Note that the order of resources doesn't matter - managed_directory will
+# not clean up this file.
+directory "#{testdir}/b_dir" do
+  action :create
+end
+
+# At the end of a Chef run containing this recipe, /tmp/foo should contain
+# files "a" and "b" only.  File "c" will have been removed.
---
+++ spec/unit/recipes/test_directories_spec.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: managed_directory
+# Spec:: test_directories
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'managed_directory::test_directories' do
+  context 'When all attributes are default, on CentOS 6.6' do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new(
+        step_into: ['managed_directory'],
+        platform: 'centos',
+        version: '6.6'
+      ) do |node|
+        # Set/override attributes here
+      end
+      runner.converge(described_recipe)
+    end
+
+    before do
+      testdir_contents = [
+        '/tmp/foo/a_dir',
+        '/tmp/foo/b_dir',
+        '/tmp/foo/c_dir'
+      ]
+      allow(Dir).to receive(:glob).and_call_original
+      allow(Dir).to receive(:glob).with('/tmp/foo/*').and_return(testdir_contents)
+      allow(File).to receive(:directory?).and_call_original
+      allow(File).to receive(:directory?).with('/tmp/foo/a_dir').and_return(true)
+      allow(File).to receive(:directory?).with('/tmp/foo/b_dir').and_return(true)
+      allow(File).to receive(:directory?).with('/tmp/foo/c_dir').and_return(true)
+    end
+
+    it 'should not remove directory a_dir' do
+      expect(chef_run).to_not delete_directory('/tmp/foo/a_dir')
+    end
+
+    it 'should not remove directory b_dir' do
+      expect(chef_run).to_not delete_directory('/tmp/foo/b_dir')
+    end
+
+    it 'should remove directory c_dir' do
+      expect(chef_run).to delete_directory('/tmp/foo/c_dir')
+    end
+  end
+end
