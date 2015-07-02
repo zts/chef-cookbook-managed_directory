57fc6309dda5b00ff94932fdd909574e751279c1
Gregory Ruiz-Ade <gkra@unnerving.org>
2015-07-02 10:13:20 -0700

    Update version for release

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "zts@cryptocracy.com"
 license          "Apache 2.0"
 description      "Provides the managed_directory resource"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
 source_url       'https://github.com/zts/chef-cookbook-managed_directory'
 issues_url       'https://github.com/zts/chef-cookbook-managed_directory/issues'
+version          "0.2.0"
